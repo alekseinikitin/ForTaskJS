b8c0020ffb2e967c1d9b7db6fbcae2c6e67e09d0
alekseinikitin <alekseinikitin.spb@gmail.com>
2024-04-02 18:47:54 +0300

    3
    3

--- Tasks/task10.js
+++ Tasks/task10.js
@@ -2,6 +2,7 @@
 
 Пеример:
 var url = "http://www.ozon.ru/context/detail/id/19677670/"
-Домен: www.ozon.ru
 
-console.log(url.slice(7, 18));
+let url2 = url.split ('/');
+console.log(url2);
+console.log(url2.slice(2,3));
